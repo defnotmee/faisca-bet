a08a47dfa083e9dc54f9b36c4c916d2c24138451
defnotmee <leonardovncomp@gmail.com>
2024-06-22 13:44:17 -0300

    arrumando big decimal

--- src/Contas/Conta.java
+++ src/Contas/Conta.java
@@ -3,14 +3,14 @@ package Contas;
 import java.math.BigDecimal;
 
 public abstract class Conta {
-    String name;
-    Long id;
-    BigDecimal balance;
-    BigDecimal qtdDeposit;
-    BigDecimal qtdBonus;
-    BigDecimal qtdLoss;
+    private String name;
+    private Long id;
+    private BigDecimal balance;
+    private BigDecimal qtdDeposit;
+    private BigDecimal qtdBonus;
+    private BigDecimal qtdLoss;
 
-    BigDecimal getUserProfit(){
-        return balance-qtdDeposit-qtdBonus;
+    public BigDecimal getUserProfit(){
+        return balance.subtract(qtdDeposit.add(qtdBonus));
     }
 }
