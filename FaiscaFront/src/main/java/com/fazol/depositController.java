55be602c2f5007b9a249718b94a4a3f1e5f9082b
TheBorts <bortolettolucas2005@gmail.com>
2024-06-25 18:33:22 -0300

    Updating deposit to make requests

--- FaiscaFront/src/main/java/com/fazol/depositController.java
+++ FaiscaFront/src/main/java/com/fazol/depositController.java
@@ -1,9 +1,16 @@
 package com.fazol;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fazol.Requester.RequesterDeposit;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 
 public class depositController {
     
@@ -26,9 +33,30 @@ public class depositController {
     void tryDeposito(ActionEvent event) {
         try {
             if (Ammount.getText().isEmpty() || Double.parseDouble(Ammount.getText()) <= 0) {
-                System.out.println("Valor inválido");
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("ERRO");
+                alert.setHeaderText("Valor inválido");
+                alert.setContentText("Por favor insira um valor valido para o depósito!");
+                alert.showAndWait();
                 return;
             }
+            
+            RequesterDeposit requesterDeposit = new RequesterDeposit();
+            List <String> args = new ArrayList<String>();
+            args.add(String.valueOf(App.conta.getId()));
+            args.add(Ammount.getText());
+
+
+            boolean bonus = requesterDeposit.makeRequest(args);
+
+            if (bonus){
+                Alert bonusAlert = new Alert(AlertType.INFORMATION);
+                bonusAlert.setTitle("PARABENSS!");
+                bonusAlert.setHeaderText("Você ganhou um bônus!");
+                bonusAlert.setContentText("Ao depositar mais de 100 reais você recebeu um bônus único de mais 100 reais. Aproveite!");
+                bonusAlert.showAndWait();
+            }
+
         } catch (Exception e) {
             System.out.println("Dados mal formatados");
             return;
